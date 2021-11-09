8f4efcdafac7cdb6fceebb57f185e584fd10d13d
Alif Khaerudin <alifkha7@live.com>
2021-11-09 21:55:48 +0700

    Routing dan Basic Controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/coba-routing', function () {
+    return 'Ini adalah hasil percobaan routing';
+});
